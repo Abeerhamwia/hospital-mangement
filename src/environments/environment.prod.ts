d44de9f0e978a03cff374a1995ad242074acba58
Abeer Alhamwia <abeerhamwia@gmail.com>
2023-04-23 19:14:13 +0300

    Done

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebaseConfig : {
+        apiKey: "AIzaSyA2uy7oUOGpXonwVPYbl9zEEArO8j5q3k0",
+        authDomain: "hospital-management-4ee40.firebaseapp.com",
+        projectId: "hospital-management-4ee40",
+        storageBucket: "hospital-management-4ee40.appspot.com",
+        messagingSenderId: "248275707902",
+        appId: "1:248275707902:web:985cb336192ef836e9c2af"
+      }
+  };
